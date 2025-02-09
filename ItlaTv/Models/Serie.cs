ec27a4469242f62fb2687fdfcabb3e1f12a48b98
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-02-09 03:34:56 -0400

    Añadir propiedades a Serie y estilos en site.css
    Se ha modificado la declaración de `using` en `Serie.cs` para incluir `System.ComponentModel` y `System.ComponentModel.DataAnnotations`.
    
    Se han añadido varias propiedades a la clase `Serie` con atributos `DisplayName` para proporcionar nombres descriptivos:
    - `EnlaceVideo` con el nombre "Link del Trailer".
    - `ProductoraId` con el nombre "Productora".
    - `GeneroPrimarioId` con el nombre "Genero Primario".
    - `GeneroSecundarioId` con el nombre "Genero Secundario".
    
    En `site.css`, se ha añadido un estilo para los elementos de formulario `input` de tipo `text` y `select` para que tengan un ancho del 25% con la propiedad `!important`.

--- ItlaTv/Models/Serie.cs
+++ ItlaTv/Models/Serie.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ItlaTv.Models
 {
@@ -13,16 +14,20 @@ namespace ItlaTv.Models
         public string? Imagen { get; set; }
 
         [Required]
+        [DisplayName("Link del Trailer")]
         public string? EnlaceVideo { get; set; }
 
         [Required]
+        [DisplayName("Productora")]
         public int ProductoraId { get; set; }
         public Productora? Productora { get; set; }
 
         [Required]
+        [DisplayName("Genero Primario")]
         public int GeneroPrimarioId { get; set; }
         public Genero? GeneroPrimario { get; set; }
 
+        [DisplayName("Genero Secundario")]
         public int? GeneroSecundarioId { get; set; }
         public Genero? GeneroSecundario { get; set; }
     }
